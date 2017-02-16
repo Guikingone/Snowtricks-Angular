f59ef9d8cd1291db6a8a67bb11afeede57e6a9ac
Guikingone <guillaume.loulier@hotmail.fr>
2017-02-16 15:52:20 +0100

    [+](Main structure)[!M][Core] || Main structure|Progress

---
+++ src/app/NotFound/notFound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-notFound',
+    templateUrl: './notFound.component.html',
+    styleUrls: ['./notFound.component.css']
+})
+
+export class NotFoundComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./Home/home.component";
+import { RegisterComponent } from "./Register/register.component";
+import { LoginComponent } from "./Login/login.component";
+
+// Errors components
+import { NotFoundComponent } from "./NotFound/notFound.component";
+
+const routes: Routes = [
+  // Homepage redirection.
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  // Main component.
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+
+    // Only for 404 purpose !
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
